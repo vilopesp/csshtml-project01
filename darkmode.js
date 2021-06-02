c6c05947d9b598e9581d10409f6a6b474dd0786a
vilopesp <vilopesp@hotmail.com>
2021-06-02 09:46:18 -0300

    working dark mode

--- darkmode.js
+++ darkmode.js
@@ -1,6 +1,17 @@
+// Select the button/checkbox
 const chk = document.querySelector("#chk");
 
+// Listen for a click on the button/checkbox
 chk.addEventListener('change', () => {
-    document.body.classList.toggle('dark');
-    document.classList.toggle('dark');
+    // Toggle (add/remove) the .dark-theme class to the body
+    document.body.classList.toggle('dark-theme');
+})
+
+// Select the button
+const btn = document.querySelector('.btn-toggle');
+
+// Listen for a click on the button
+btn.addEventListener('click', function() {
+  // Then toggle (add/remove) the .dark-theme class to the body
+  document.body.classList.toggle('dark-theme');  
 })
