f486ee7814e6fd68a6d98f2386fda01744e23c7c
vilopesp <vilopesp@hotmail.com>
2021-06-02 09:48:22 -0300

    correction on js file

--- darkmode.js
+++ darkmode.js
@@ -6,12 +6,3 @@ chk.addEventListener('change', () => {
     // Toggle (add/remove) the .dark-theme class to the body
     document.body.classList.toggle('dark-theme');
 })
-
-// Select the button
-const btn = document.querySelector('.btn-toggle');
-
-// Listen for a click on the button
-btn.addEventListener('click', function() {
-  // Then toggle (add/remove) the .dark-theme class to the body
-  document.body.classList.toggle('dark-theme');  
-})
